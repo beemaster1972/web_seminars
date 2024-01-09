caed5f8d986403a5e46dc30740da75b7e2b52de5
beemaster <ugor.finno@yandex.ru>
2024-01-09 22:53:13 +0500

    Add index.js

---
+++ Seminar03/script/index.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".dt").forEach(function (el) {
+    el.style.color = "blue";
+  });
+  // let n1 = "Что-нибудь";
+  // document.write(`строковый тип + ${n1}`);
+  // const n2 = "2312312"
+  // console.log(3123123)
+  // alert("dawdaw")
+  document.querySelector("a").style.color = "#DC143C";
