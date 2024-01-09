7f25351889745b81cc1db2b2b54b19bd113c7217
beemaster <ugor.finno@yandex.ru>
2024-01-10 00:23:37 +0500

    Add <dark theme> task

---
+++ Seminar03/blackTheme/script/main.js
@@ -0,0 +1,5 @@
+let toggle_button = document.getElementById("toggle_button")
+toggle_button.addEventListener("click", function()
+{
+    document.body.classList.toggle("dark")
+})
---
+++ Seminar03/task01.js
@@ -0,0 +1,48 @@
+// let a = 158
+// let b = 2
+// document.write(a+b)
+
+
+// let prmt = "Привет, Алевтина!"
+// confirm(prmt)
+
+
+//let my_name = prompt("Как вас зовут?", "Имя: ")
+//document.write(my_name)
+// let alevtina = "Алевтина"
+
+// function printVar(){
+    
+//     document.write(`Привет, ${alevtina}`)
+
+// }
+
+// printVar()
+
+// let userAnswer = confirm("Вы уверены?")
+// if (userAnswer) alert("Мы рады, что Вы уверены!")
+// else alert("Жаль, что Вы не уверены…")
+
+// let age = parseInt(prompt("Введите возвраст"));
+//     switch (age) {
+//         case 18:  
+//             alert('Вы совершеннолетний, все можно!')
+//             break
+//         case 10:
+//             alert('Вам надо учить уроки!');
+//             break
+//         case 30:
+//             alert('Ложитесь спать, завтра на работу');
+//             break
+//         default:
+//             alert('Мы не знаем что Вам делать');
+//     }
+
+let myArray = [1,3,5,7,9,12,31,25,78,100]
+document.writeln(myArray)
+for (i=0;i<myArray.length;i++) {
+    myArray[i] += 1
+}
+document.write(myArray)
+
+
